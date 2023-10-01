9aa5ff7eb25b4fa1e3592e9d180f41338213bb94
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2023-10-01 23:54:50 +0900

    feat: 회워가입, 로그인 로직 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,7 +1,13 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,27 +19,129 @@ public class RequestHandler implements Runnable{
         this.connection = connection;
     }
 
-    @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String requestLine = br.readLine();
+            if (requestLine == null) return;
+
+            int requestContentLength = getRequestContentLength(br);
+
+            if (requestLine.startsWith("POST /user/signup") || requestLine.startsWith("POST /user/login")) {
+                handlePostRequest(requestLine, requestContentLength, br, dos);
+            } else if (requestLine.startsWith("GET /")) {
+                handleGetRequest(br, dos, requestLine);
+            }
 
         } catch (IOException e) {
-            log.log(Level.SEVERE,e.getMessage());
+            log.log(Level.SEVERE, "An error occurred: ", e);
         }
     }
 
+    private int getRequestContentLength(BufferedReader br) throws IOException {
+        int contentLength = 0;
+        while (true) {
+            final String line = br.readLine();
+            if (line.equals("")) {
+                break;
+            }
+            if (line.startsWith("Content-Length")) {
+                contentLength = Integer.parseInt(line.split(": ")[1]);
+            }
+        }
+        return contentLength;
+    }
+
+    private void handlePostRequest(String requestLine, int requestContentLength, BufferedReader br, DataOutputStream dos) throws IOException {
+        char[] body = new char[requestContentLength];
+        br.read(body, 0, requestContentLength);
+        String requestBody = new String(body);
+        Map<String, String> params = parseQueryString(requestBody);
+
+        if (requestLine.startsWith("POST /user/signup")) {
+            User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+            MemoryUserRepository.getInstance().addUser(user);
+            response302Header(dos, "/index.html");
+        } else if (requestLine.startsWith("POST /user/login")) {
+            handleLoginRequest(params, dos);
+        }
+    }
+
+    private void handleGetRequest(BufferedReader br, DataOutputStream dos, String requestLine) throws IOException {
+
+        // 사용자가 /user/list에 접근 시도
+        if (requestLine.startsWith("GET /user/list.html")) {
+            // 로그인 상태 확인
+            if (isLogined(br)) {
+                // list.html 파일의 경로를 응답으로 반환
+                String filePath = "webapp/list.html";
+                byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+                response200Header(dos, fileBytes.length);
+                responseBody(dos, fileBytes);
+            } else {
+                // 로그인 페이지로 리다이렉트
+                response302Header(dos, "/login.html");
+            }
+            return;
+        }
+
+        String filePath = "webapp" + requestLine.split(" ")[1];
+        if (!Files.exists(Paths.get(filePath))) {
+            response404Header(dos);  // 파일이 없으면 404 에러를 반환
+            return;
+        }
+        byte[] body = Files.readAllBytes(Paths.get(filePath));
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+
+    private void handleLoginRequest(Map<String, String> params, DataOutputStream dos) throws IOException {
+        String userId = params.get("userId");
+        String password = params.get("password");
+        User user = MemoryUserRepository.getInstance().findUserById(userId);
+
+        if (user != null && user.getPassword().equals(password)) {
+            // 로그인 성공
+            response302HeaderWithCookie(dos, "/index.html", "logined=true");
+        } else {
+            // 로그인 실패
+            response302Header(dos, "/user/logined_failed.html");
+        }
+    }
+
+    private void response302HeaderWithCookie(DataOutputStream dos, String location, String cookie) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+        dos.writeBytes("Set-Cookie: " + cookie + "; Path=/" + "\r\n");
+        dos.writeBytes("\r\n");
+    }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + location + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
@@ -49,4 +157,29 @@ public class RequestHandler implements Runnable{
         }
     }
 
+    // Query String을 파싱하는 메소드
+    private Map<String, String> parseQueryString(String queryString) {
+        Map<String, String> parameters = new HashMap<>();
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length > 1) {
+                parameters.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return parameters;
+    }
+
+    private boolean isLogined(BufferedReader br) throws IOException {
+        while (true) {
+            final String line = br.readLine();
+            if (line.equals("")) {
+                break;
+            }
+            if (line.startsWith("Cookie")) {
+                return line.contains("logined=true");
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/http/util/HttpRequestUtilsTest.java
+++ src/test/java/http/util/HttpRequestUtilsTest.java
@@ -1,8 +1,13 @@
 package http.util;
 
 import org.junit.jupiter.api.Test;
+import webserver.RequestHandler;
 
+import java.nio.file.*;
 import java.util.Map;
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 class HttpRequestUtilsTest {
@@ -23,5 +28,66 @@ class HttpRequestUtilsTest {
     @Test
     void parseQueryZero() {
         Map<String, String> queryParameter = HttpRequestUtils.parseQueryParameter("");
+    }
+
+    @Test
+    void 리다이렉트_302() throws IOException {
+        // given
+        String requestData = "POST /user/signup HTTP/1.1\r\n" +
+                "Host: localhost\r\n" +
+                "Content-Length: 68\r\n" +
+                "\r\n";
+        InputStream inputStream = new ByteArrayInputStream(requestData.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Socket socket = new MockSocket(inputStream, outputStream);
+        RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        String response = new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+        assertTrue(response.contains("HTTP/1.1 302 Found"));
+        assertTrue(response.contains("Location: /index.html"));
+    }
+    @Test
+    void 유저_생성시_리다이렉트() throws IOException {
+        // given
+        String requestData = "POST /user/signup HTTP/1.1\r\n" +
+                "Host: localhost\r\n" +
+                "Content-Length: 68\r\n" +
+                "\r\n" +
+                "userId=javajigi&password=password&name=JaeSung&email=javajigi%40slipp.net";
+        InputStream inputStream = new ByteArrayInputStream(requestData.getBytes(StandardCharsets.UTF_8));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Socket socket = new MockSocket(inputStream, outputStream);
+        RequestHandler requestHandler = new RequestHandler(socket);
+
+        // when
+        requestHandler.run();
+
+        // then
+        String response = new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+        assertTrue(response.contains("HTTP/1.1 302 Found"));
+        assertTrue(response.contains("Location: /index.html"));
+    }
+
+
+    private static class MockSocket extends Socket {
+        private final InputStream input;
+        private final OutputStream output;
+
+        public MockSocket(InputStream input, OutputStream output) {
+            this.input = input;
+            this.output = output;
+        }
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return input;
+        }
+        @Override
+        public OutputStream getOutputStream() throws IOException {
+            return output;
+        }
     }
 }
